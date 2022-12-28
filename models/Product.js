6e369276e60fecb3161a26d5bde88ac624e1a140
ميرنا الشريف <merna.m.elsherief@gmail.com>
2022-12-28 17:41:48 +0200

    modify

--- models/Product.js
+++ models/Product.js
@@ -5,9 +5,7 @@ const ProductSchema = new mongoose.Schema(
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
-    categories: { type: Array },
-    size: { type: String },
-    color: { type: String },
+    inStock: { type: Boolean, default: true },
     price: { type: Number, required: true },
   },
   { timestamps: true }
